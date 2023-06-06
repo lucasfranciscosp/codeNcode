8144c5d0e028a6b4a2d17c3fc3fafadc1b6f463c
lucasfranciscosp <lucasfranciscosp7@gmail.com>
2023-06-06 00:25:17 -0300

    aula 3

--- Graphs/l03.cpp
+++ Graphs/l03.cpp
@@ -1,18 +1,20 @@
 #include <bits/stdc++.h>
 #define _ ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 #define endl '\n'
-#define f first
-#define s second
-#define ou ||
 #define dbg(x) cout << #x << " = " << x << endl
-#define forn(i, n) for (int i = 0; i < int(n); i++)
 typedef long long ll;
 const int INF = 0x3f3f3f3f;
 const ll LINF = 0x3f3f3f3f3f3f3f3fll;
  
 using namespace std;
 
-//Depth First Search (DFS)
+// Counting connected components, ptbr = Contando componentes conexos
+
+// Just use DFS for each i which is not visited
+// when the graph is not direcional
+
+// Só usar DFS pra cada vertice i, como ele vai percorrer o grafo inteiro
+// Basta fazer uma variável que soma toda vez que chamar a dfs
 
 vector<int> g[1001], vis(1001, 0);
 
@@ -31,11 +33,20 @@ int main(){_
     // M = num edges , ptbr = arestas
     int n, m ;
 
+    int cc_count = 0;
+    
     while(m--){
         int a,b;
         cin>>a>>b;
         g[a].push_back(b);
         g[b].push_back(a);
     }
-    dfs(1);
+
+    for(int i=1; i <= n;i++){
+        if(!vis[i]){
+            dfs(i);
+            cc_count += 1;
+        }
+    }
+    cout<<cc_count<<endl;
 }
